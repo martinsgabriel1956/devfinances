c4eb971bce3b4e5643c035b7043cce2f459a77cf
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-04-04 22:41:03 -0300

    code refactoring completed :tada:

--- scripts.js
+++ scripts.js
@@ -1,31 +1,26 @@
 Modal = {
-  open() {
-    document.querySelector(".modal-overlay").classList.add("active");
-  },
-  close() {
-    document.querySelector(".modal-overlay").classList.remove("active");
-  },
+  open: () => document.querySelector(".modal-overlay").classList.add("active"),
+
+  close: () => document.querySelector(".modal-overlay").classList.remove("active"),
 };
 
 const Storage = {
-  get() {
-    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
-  },
-  set(transactions) {
-    localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
-  }
+  get: () => JSON.parse(localStorage.getItem("dev.finances:transactions")) || [],
+  
+  set: transactions => localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions)),
+
 }
 
 const Transaction = {
   all: Storage.get(),
 
-  add(transaction) {
+  add: transaction => {
     Transaction.all.push(transaction);
 
     App.reload();
   },
 
-  remove(index) {
+  remove: index => {
     Transaction.all.splice(index, 1);
 
     App.reload();
@@ -35,12 +30,9 @@ const Transaction = {
     let income = 0;
     // Pegar todas as transações
     // Para cada transação
-    Transaction.all.forEach((transaction) => {
-      //se for maior que zero
-      if (transaction.amount > 0) {
-        // somar a uma variável e retornar a variável
-        income += transaction.amount;
-      }
+    Transaction.all.forEach(transaction => {
+      //se for maior que zero, soma a uma variável e retornar a variável
+      if (transaction.amount > 0) income += transaction.amount;
     });
     return income;
   },
@@ -50,25 +42,20 @@ const Transaction = {
     // Pegar todas as transações
     // Para cada transação
     Transaction.all.forEach((transaction) => {
-      //se for maior que zero
-      if (transaction.amount < 0) {
-        // somar a uma variável e retornar a variável
-        expense += transaction.amount;
-      }
+      //se for maior que zero, soma a uma variável e retornar a variável
+      if (transaction.amount < 0) expense += transaction.amount;
     });
     return expense;
   },
 
-  total() {
-    return Transaction.incomes() + Transaction.expenses();
-  },
+  total: () => Transaction.incomes() + Transaction.expenses(),
 };
 
 // Substituir os dados do HTML com os dados os JS
 const DOM = {
   transactionsContainer: document.querySelector("#data-table tbody"),
 
-  addTransaction(transaction, index) {
+  addTransaction: (transaction, index) => {
     const tr = document.createElement("tr");
     tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
     tr.dataset.index = index;
@@ -76,7 +63,7 @@ const DOM = {
     DOM.transactionsContainer.appendChild(tr);
   },
 
-  innerHTMLTransaction(transaction, index) {
+  innerHTMLTransaction: (transaction, index) => {
     const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
     const amount = Utils.formatCurrency(transaction.amount);
@@ -95,35 +82,29 @@ const DOM = {
     return html;
   },
 
-  updateBalance() {
-    document.querySelector("#incomeDisplay").innerHTML = Utils.formatCurrency(
-      Transaction.incomes()
-    );
-    document.querySelector("#expenseDisplay").innerHTML = Utils.formatCurrency(
-      Transaction.expenses()
-    );
-    document.querySelector("#totalDisplay").innerHTML = Utils.formatCurrency(
-      Transaction.total()
-    );
+  updateBalance: () => {
+    document.querySelector("#incomeDisplay").innerHTML = Utils.formatCurrency(Transaction.incomes());
+    
+    document.querySelector("#expenseDisplay").innerHTML = Utils.formatCurrency(Transaction.expenses());
+    
+    document.querySelector("#totalDisplay").innerHTML = Utils.formatCurrency(Transaction.total());
   },
 
-  clearTransactions() {
-    DOM.transactionsContainer.innerHTML = "";
-  },
+  clearTransactions: () => DOM.transactionsContainer.innerHTML = "",
 };
 
 const Utils = {
-  formatAmount(value) {
+  formatAmount: value => {
     value = Number(value) * 100;
     return value;
   },
 
-  formatDate(date) {
+  formatDate: date => {
     const splittedDate = date.split("-");
     return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
   },
 
-  formatCurrency(value) {
+  formatCurrency: value => {
     const signal = Number(value) < 0 ? "-" : "";
 
     value = String(value).replace(/\D/g, "");
@@ -144,7 +125,7 @@ const Form = {
   amount: document.querySelector("input#amount"),
   date: document.querySelector("input#date"),
 
-  getValues() {
+  getValues: () => {
     return {
       description: Form.description.value,
       amount: Form.amount.value,
@@ -152,18 +133,13 @@ const Form = {
     };
   },
 
-  validateFields() {
+  validateFields: () => {
     const { description, amount, date } = Form.getValues();
-    if (
-      description.trim() === "" ||
-      amount.trim() === "" ||
-      date.trim() === ""
-    ) {
-      throw new Error("Por favor, preencha todos os campos!");
-    }
+   
+    if (description.trim() === "" || amount.trim() === "" || date.trim() === "") throw new Error("Por favor, preencha todos os campos!");
   },
 
-  formatValues() {
+  formatValues: () => {
     let { description, amount, date } = Form.getValues();
 
     amount = Utils.formatAmount(amount);
@@ -176,13 +152,13 @@ const Form = {
     };
   },
 
-  clearFields() {
+  clearFields: () => {
     Form.description.value = "";
     Form.amount.value = "";
     Form.date.value = "";
   },
 
-  submit(event) {
+  submit: event => {
     event.preventDefault();
 
     try {
@@ -203,14 +179,14 @@ const Form = {
 };
 
 const App = {
-  init() {
+  init: () => {
     Transaction.all.forEach(DOM.addTransaction);
 
     DOM.updateBalance();
 
     Storage.set(Transaction.all);
   },
-  reload() {
+  reload: () => {
     DOM.clearTransactions();
     App.init();
   },
